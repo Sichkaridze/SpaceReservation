89f19ac2227f134c0c32b7c9d625c700104b3db8
Sichkaridze <dima.sichkar2003@gmail.com>
2025-02-23 14:31:28 +0200

    Create basic permission for project

--- src/planetarium/permissions.py
+++ src/planetarium/permissions.py
@@ -1,6 +1,13 @@
-from rest_framework import permissions
+from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
-class IsOwnerOrAdmin(permissions.BasePermission):
+class IsOwnerOrAdmin(BasePermission):
     def has_object_permission(self, request, view, obj):
         return request.user == obj.user or request.user.is_staff
+
+
+class IsAdminUserOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+        return request.user.is_staff
