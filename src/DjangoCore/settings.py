a8b12fe0d379d8dc30f674bb4678e34f989ba8f3
Sichkaridze <dima.sichkar2003@gmail.com>
2025-02-23 22:27:26 +0200

    Delete extra

--- src/DjangoCore/settings.py
+++ src/DjangoCore/settings.py
@@ -146,7 +146,6 @@ DATETIME_FORMAT = 'd b Y - H:i:s'
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
